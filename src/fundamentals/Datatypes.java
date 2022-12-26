283e3708263a5b974061e8817476b7a7eba24bee
KarthikBenki <karthikmc007@gmail.com>
2022-12-26 09:37:27 +0530

    variables and datatypes

---
+++ src/fundamentals/Datatypes.java
@@ -0,0 +1,26 @@
+package fundamentals;
+
+public class Datatypes {
+    public static void main(String[] args) {
+        //integer number
+        int a = 10;
+        short b = 20;
+        byte c = 11;
+        long l = 117;
+
+        //decimal
+        double i = 10.5;//8 bytes
+        float f = 10.5f;//4 bytes
+
+        //characters
+        char x = 'a'; //1 byte
+
+        boolean bool = true;
+        boolean bool1 = false;
+
+
+
+
+
+    }
+}

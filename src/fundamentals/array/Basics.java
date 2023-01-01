03373339a2d03dc2caef5882b137f2fcb033ab19
KarthikBenki <karthikmc007@gmail.com>
2023-01-01 17:50:54 +0530

    input of Array

--- src/fundamentals/array/Basics.java
+++ src/fundamentals/array/Basics.java
@@ -1,7 +1,11 @@
 package fundamentals.array;
 
+import java.util.Arrays;
+
 public class Basics {
     public static void main(String[] args) {
         int arr[] = new int[5];
+        Arrays.stream(arr).sum();
+
     }
 }
---
+++ src/fundamentals/array/InputOfArray.java
@@ -0,0 +1,28 @@
+package fundamentals.array;
+
+import java.util.Scanner;
+
+public class InputOfArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int arr[] = new int[n];
+
+        takeInput(scanner, arr);
+
+        printArray(n, arr);
+
+    }
+
+    private static void printArray(int n, int[] arr) {
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    private static void takeInput(Scanner scanner, int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scanner.nextInt();
+        }
+    }
+}

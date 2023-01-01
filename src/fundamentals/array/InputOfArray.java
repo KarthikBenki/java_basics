f9fd936c565556ed875dc175a8f3586ccd917b9c
KarthikBenki <karthikmc007@gmail.com>
2023-01-01 22:48:16 +0530

    input of array

--- src/fundamentals/array/InputOfArray.java
+++ src/fundamentals/array/InputOfArray.java
@@ -14,7 +14,7 @@ public class InputOfArray {
 
     }
 
-    private static void printArray(int n, int[] arr) {
+    public static void printArray(int n, int[] arr) {
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
         }

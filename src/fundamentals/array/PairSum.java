0d15bbc662636f086a5f7652baf20ff32d6b5114
KarthikBenki <karthikmc007@gmail.com>
2023-01-02 10:01:23 +0530

    pair sum count  of an array

---
+++ src/fundamentals/array/PairSum.java
@@ -0,0 +1,30 @@
+package fundamentals.array;
+
+import java.util.Scanner;
+
+import static fundamentals.array.InputOfArray.takeInput;
+
+public class PairSum {
+
+    public static int pairSum(int arr[], int x) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1 ; j < arr.length; j++) {
+                if (arr[i] + arr[j] == x ) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int arr[] = new int[n];
+        takeInput(scanner, arr);
+        int x = scanner.nextInt();
+        System.out.println(pairSum(arr, x));
+
+    }
+}

b0b926c8b78dabad011fabc5f44df6c2f5660564
KarthikBenki <karthikmc007@gmail.com>
2023-01-02 11:18:05 +0530

    sort zeros and ones

---
+++ src/fundamentals/array/SortZerosAndOnes.java
@@ -0,0 +1,35 @@
+package fundamentals.array;
+
+import java.util.Scanner;
+
+import static fundamentals.array.InputOfArray.printArray;
+import static fundamentals.array.InputOfArray.takeInput;
+
+public class SortZerosAndOnes {
+    public static void sortZeroesAndOne(int[] arr) {
+        int zeroCount = 0;
+        int oneCount = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]==0) zeroCount++;
+            else oneCount++;
+        }
+
+        for (int i = 0; i < zeroCount; i++) {
+            arr[i] = 0;
+        }
+
+        for (int i = zeroCount; i < arr.length; i++) {
+            arr[i] = 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int arr[] = new int[n];
+        takeInput(scanner, arr);
+        sortZeroesAndOne(arr);
+        printArray(arr.length,arr);
+    }
+}

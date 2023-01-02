9a1399ec6480db8a4e8654765cee79ccd8972a83
KarthikBenki <karthikmc007@gmail.com>
2023-01-02 19:44:52 +0530

    insertion sort

---
+++ src/fundamentals/array/searchingandsorting/InsertionSortForLoop.java
@@ -0,0 +1,38 @@
+package fundamentals.array.searchingandsorting;
+
+import java.util.Scanner;
+
+import static fundamentals.array.InputOfArray.printArray;
+import static fundamentals.array.InputOfArray.takeInput;
+
+
+public class InsertionSortForLoop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the size of an array");
+        int n = scanner.nextInt();
+
+        int arr[] = new int[n];
+
+        takeInput(scanner, arr);
+        System.out.println("Before sorting....");
+        printArray(arr);
+        insertionSort(arr);
+
+        System.out.println("After sorting....");
+        printArray(arr);
+
+    }
+
+    public static void insertionSort(int[] arr) {
+        int temp;
+        for (int i = 1; i < arr.length; i++) {
+            temp = arr[i];
+            int j = i - 1;
+            for (; j >= 0 && arr[j] > temp; j--) {
+                arr[j + 1] = arr[j];
+            }
+            arr[j + 1] = temp;
+        }
+    }
+}
---
+++ src/fundamentals/array/searchingandsorting/InsertionSortWhileLoop.java
@@ -0,0 +1,41 @@
+package fundamentals.array.searchingandsorting;
+
+import java.util.Scanner;
+
+import static fundamentals.array.InputOfArray.printArray;
+import static fundamentals.array.InputOfArray.takeInput;
+
+
+public class InsertionSortWhileLoop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the size of an array");
+        int n = scanner.nextInt();
+
+        int arr[] = new int[n];
+
+        takeInput(scanner, arr);
+        System.out.println("Before sorting....");
+        printArray(arr);
+        insertionSort(arr);
+
+        System.out.println("After sorting....");
+        printArray(arr);
+
+    }
+
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int temp,j;
+            temp = arr[i];
+            j=i;
+            while (j>0 && arr[j-1]>temp){
+                arr[j] = arr[j-1];
+                j--;
+            }
+            arr[j] = temp;
+        }
+    }
+
+
+}

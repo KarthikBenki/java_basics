84810fcde5a49f0902f33a656e69a9ac27604445
KarthikBenki <karthikmc007@gmail.com>
2023-01-02 08:58:21 +0530

    find duplicate in an array

---
+++ src/fundamentals/array/FindDuplicateInAnArray.java
@@ -0,0 +1,16 @@
+package fundamentals.array;
+
+public class FindDuplicateInAnArray {
+    public static int duplicateNumber(int arr[]) {
+
+        for (int ele1:arr){
+            int count  = 0;
+            for (int ele2:arr){
+                if (ele2==ele1) count++;
+            }
+            if (count==2) return ele1;
+        }
+
+        return 0;
+    }
+}

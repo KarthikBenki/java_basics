6fb8a75a7a01b17fa2b5ea807fd0792ef2e9d468
KarthikBenki <karthikmc007@gmail.com>
2023-01-03 23:55:38 +0530

    rotate array k times

---
+++ src/fundamentals/array/searchingandsorting/RotateArrayKTimes.java
@@ -0,0 +1,41 @@
+package fundamentals.array.searchingandsorting;
+
+import java.util.Scanner;
+
+import static fundamentals.array.InputOfArray.printArray;
+import static fundamentals.array.InputOfArray.takeInput;
+
+public class RotateArrayKTimes {
+    public static void rotate(int[] arr, int d) {
+        int n = arr.length;
+        for (int i = 0, j = d - 1; i < j; i++, j--) {
+            swap(arr, i, j);
+        }
+
+        for (int i = d, j = n - 1; i < j; i++, j--) {
+            swap(arr, i, j);
+        }
+
+        for (int i = 0, j = n - 1; i < j; i++, j--) {
+            swap(arr, i, j);
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int arr[] = new int[n];
+        takeInput(scanner,arr);
+        int d = scanner.nextInt();
+        printArray(arr);
+        rotate(arr,d);
+        printArray(arr);
+
+    }
+}

f871569b4be9d24f88423c3cd5a2366a170583fd
KarthikBenki <karthikmc007@gmail.com>
2023-01-04 15:05:27 +0530

    push zeros to end

---
+++ src/fundamentals/array/searchingandsorting/PushZerosToEnd.java
@@ -0,0 +1,37 @@
+package fundamentals.array.searchingandsorting;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static fundamentals.array.InputOfArray.takeInput;
+
+public class PushZerosToEnd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int arr[] = new int[n];
+
+        takeInput(scanner,arr);
+
+        pushZerosToEnd(arr);
+        System.out.println(Arrays.toString(arr));
+
+
+    }
+
+    private static void pushZerosToEnd(int[] arr) {
+        int temp[] = new int[arr.length];
+        int j = 0;
+        for (int i = 0;i<arr.length; i++){
+            if (arr[i] > 0) {
+                temp[j++] = arr[i];
+            }
+        }
+
+        for (int i = 0,k=0; i < arr.length; i++,k++) {
+            arr[i] = temp[k];
+        }
+    }
+
+
+}
---
+++ src/fundamentals/array/searchingandsorting/PushZerosToEnd1.java
@@ -0,0 +1,36 @@
+package fundamentals.array.searchingandsorting;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static fundamentals.array.InputOfArray.takeInput;
+
+public class PushZerosToEnd1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int arr[] = new int[n];
+
+        takeInput(scanner,arr);
+
+        pushZerosToEnd(arr);
+        System.out.println(Arrays.toString(arr));
+
+
+    }
+
+    private static void pushZerosToEnd(int[] arr) {
+        int j = 0;
+        for (int i = 0;i<arr.length; i++){
+
+            if (arr[i] != 0) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+    }
+
+
+}

ca426213d5737e7d88c965ed0e7186d22480d3d6
KarthikBenki <karthikmc007@gmail.com>
2023-01-04 22:05:34 +0530

    2d array take input and give output

---
+++ src/fundamentals/array2d/TakeInputAndGiveOutput2DArray.java
@@ -0,0 +1,34 @@
+package fundamentals.array2d;
+
+import java.util.Scanner;
+
+public class TakeInputAndGiveOutput2DArray {
+    public static void takeInput(Scanner scanner, int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = scanner.nextInt();
+            }
+        }
+    }
+
+    public static void print2DArray(int[][] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int rows = scanner.nextInt();
+        int cols = scanner.nextInt();
+
+        int [][] arr = new int[rows][cols];
+
+        takeInput(scanner,arr);
+        print2DArray(arr);
+
+    }
+}

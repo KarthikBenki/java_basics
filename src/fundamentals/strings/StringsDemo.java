b946441aeded7fbb7f3e3bc9acb1462d2b21f943
KarthikBenki <karthikmc007@gmail.com>
2023-01-10 18:38:02 +0530

    strings scanner input

--- src/fundamentals/strings/StringsDemo.java
+++ src/fundamentals/strings/StringsDemo.java
@@ -1,26 +1,35 @@
 package fundamentals.strings;
 
+import java.util.Scanner;
+
 public class StringsDemo {
     public static void main(String[] args) {
-        String str1 = "Coding";
-        String str2 = "Ninjas";
-//        str1 = str1+str2;
-        str1 = str1.concat(str2);
-        System.out.println(str1);
-        System.out.println(str1.equals(str2));
+//        String str1 = "Coding";
+//        String str2 = "Ninjas";
+////        str1 = str1+str2;
+//        str1 = str1.concat(str2);
+//        System.out.println(str1);
+//        System.out.println(str1.equals(str2));
+//
+//        String s2 = "Hello";
+//        String s1 = new String("Hello");
+//        System.out.println(s1.equals(s2));
+//        System.out.println(s1 == s2);
+//        System.out.println(s1.compareTo(s2));
+//        System.out.println(str1.contains("ing"));
+//        String a ="abcd";
+//        String b="abcda";
+//        System.out.println(a.compareTo(b));
+//
+//        System.out.println(str1.length());
+//        System.out.println(str1.substring(12));
+//        System.out.println(str1.substring(1,5).length());
 
-        String s1 = new String("Hello");
-        String s2 = "Hello";
-        System.out.println(s1.equals(s2));
-        System.out.println(s1 == s2);
-        System.out.println(s1.compareTo(s2));
-        System.out.println(str1.contains("ing"));
-        String a ="abcd";
-        String b="abcda";
-        System.out.println(a.compareTo(b));
 
-        System.out.println(str1.length());
-        System.out.println(str1.substring(12));
-        System.out.println(str1.substring(1,5).length());
+        Scanner s=new Scanner(System.in);
+        String str1=s.next();
+        String str2=s.nextLine();
+        String str3=str2+str1;
+        System.out.println(str3);
     }
 }

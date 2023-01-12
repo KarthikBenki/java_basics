43b40e7b05dcfdc19ea8efc0bb6203508a3fbc16
KarthikBenki <karthikmc007@gmail.com>
2023-01-12 10:41:30 +0530

    Function functional interface

--- src/interfaces/functional/LambdaExample.java
+++ src/interfaces/functional/LambdaExample.java
@@ -10,15 +10,13 @@ class Rectangle implements Shape {
     public void draw() {
         System.out.println("rectangle draw method");
     }
-
-
 }
 
 public class LambdaExample {
 
     public static void main(String[] args) {
         Shape rectangle = () -> System.out.println("method is defined");
-            rectangle.draw();
+        rectangle.draw();
     }
 
 

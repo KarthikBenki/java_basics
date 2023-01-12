85247076d4f7a3c2a3b03358c06e6af6b73fc119
KarthikBenki <karthikmc007@gmail.com>
2023-01-12 10:45:29 +0530

    consumer interface traditional method

---
+++ src/interfaces/functional/ConsumerDemo.java
@@ -0,0 +1,20 @@
+package interfaces.functional;
+
+
+import java.util.function.Consumer;
+
+class ConsumerImpl implements Consumer<String> {
+
+    @Override
+    public void accept(String string) {
+        System.out.println(string);
+    }
+}
+
+
+public class ConsumerDemo {
+    public static void main(String[] args) {
+        Consumer<String> consumer = new ConsumerImpl();
+        consumer.accept("Hello World");
+    }
+}

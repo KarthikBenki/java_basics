7e362d3748fceefe25d584a6627cb815164b8a2b
KarthikBenki <karthikmc007@gmail.com>
2023-01-12 18:34:41 +0530

    adjust spaces

--- src/interfaces/functional/ConsumerDemo.java
+++ src/interfaces/functional/ConsumerDemo.java
@@ -16,9 +16,7 @@ public class ConsumerDemo {
     public static void main(String[] args) {
         Consumer<String> consumer = new ConsumerImpl();
         consumer.accept("Hello World");
-
         Consumer<String> con = s -> System.out.println(s);
-
         con.accept("Hello ninja");
     }
 }

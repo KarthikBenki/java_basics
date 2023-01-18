06e4492494e1033712132adcf786599bed3df4ae
KarthikBenki <karthikmc007@gmail.com>
2023-01-18 18:54:39 +0530

    Revert "multi threading using runnable interface"
    This reverts commit d60b471d6c440912201b6dae60323e8083779d61.

--- src/fundamentals/array/arraybasics/IntersectionOfTwoArrays.java
+++ src/fundamentals/array/arraybasics/IntersectionOfTwoArrays.java
@@ -10,23 +10,12 @@ public class IntersectionOfTwoArrays {
         for (int i = 0; i < arr1.length; i++) {
             for (int j = 0; j < arr2.length; j++) {
                 if (arr1[i] == arr2[j]) {
-                    System.out.print(arr1[i] + " ");
+                    System.out.print(arr1[i]+" ");
                     arr2[j] = Integer.MIN_VALUE;
                     break;
                 }
             }
         }
-    }
-
-    public static boolean isIntersect(int arr1[], int arr2[]) {
-        for (int i = 0; i < arr1.length; i++) {
-            for (int j = 0; j < arr2.length; j++) {
-                if (arr1[i] == arr2[j]) {
-                    return true;
-                }
-            }
-        }
-        return false;
     }
 
     public static void main(String[] args) {
@@ -35,14 +24,14 @@ public class IntersectionOfTwoArrays {
 
         int arr1[] = new int[n];
         takeInput(scanner, arr1);
-        printArray(arr1.length, arr1);
+        printArray(arr1.length,arr1);
         System.out.println();
 
         int m = scanner.nextInt();
 
         int arr2[] = new int[m];
         takeInput(scanner, arr2);
-        printArray(arr2.length, arr2);
+        printArray(arr2.length,arr2);
         System.out.println();
 
         intersections(arr1, arr2);
--- src/fundamentals/array/arraybasics/TripletSum.java
+++ src/fundamentals/array/arraybasics/TripletSum.java
@@ -1,8 +1,6 @@
 package fundamentals.array.arraybasics;
 
 import java.util.Scanner;
-import java.util.Arrays;
-import java.util.HashSet;
 
 import static fundamentals.array.arraybasics.InputOfArray.takeInput;
 
@@ -11,9 +9,9 @@ public class TripletSum {
     public static int findTriplet(int[] arr, int x) {
         int count = 0;
         for (int i = 0; i < arr.length; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                for (int k = j + 1; k < arr.length; k++) {
-                    if (arr[i] + arr[j] + arr[k] == x) {
+            for (int j = i+1; j < arr.length; j++) {
+                for (int k = j+1; k < arr.length; k++) {
+                    if (arr[i]+arr[j]+arr[k]==x){
                         count++;
                     }
                 }
@@ -22,101 +20,12 @@ public class TripletSum {
         return count;
     }
 
-    public static int tripletSum(int[] arr, int num) {
-        int count = 0;
-        int j, k;
-        Arrays.sort(arr);
-        for (int i = 0; i < arr.length-2; i++) {
-            j = i + 1;
-            k = arr.length - 1;
-            while (j < k) {
-                if (arr[i] + arr[j] + arr[k] == num) {
-                    count++;
-                    j++;
-                    k--;
-                }
-                else if (arr[i] + arr[j] + arr[k] < num)
-                    j++;
-
-                else // arr[i] + arr[l] + arr[r] > num
-                    k--;
-            }
-        }
-        return count;
-    }
-
-    public static int tripletSum1(int[] arr, int num) {
-        int count = 0;
-        for (int i = 0; i < arr.length - 2; i++) {
-            HashSet<Integer> s = new HashSet<>();
-            int curr_sum = num - arr[i];
-            for (int j = i + 1; j < arr.length; j++) {
-                if (s.contains(curr_sum - arr[j])) {
-                    count++;
-                }
-                s.add(arr[j]);
-            }
-        }
-        return count;
-    }
-
-    public static int tripletSum2(int[] arr, int num) {
-
-        Arrays.sort(arr);
-        int n = arr.length;
-        int numTriplets = 0;
-        for (int i=0; i<n; i++) {
-            int pairSum = num - arr[i];
-            int numPairs = 0;
-            int start = i+1;
-            int end = n-1;
-            while (start<end) {
-                if (arr[start]+arr[end]<pairSum) {
-                    start++;
-                } else if (arr[start]+arr[end]>pairSum) {
-                    end--;
-                } else {
-                    if (arr[start] == arr[end]) {
-                        int totalCount = (end - start) + 1;
-                        numPairs += (totalCount * (totalCount - 1) / 2);
-                        break;
-                    }
-
-                    int tempI = start + 1;
-                    int tempJ = end - 1;
-
-                    while (tempI <= tempJ && arr[start] == arr[tempI]) {
-                        tempI++;
-                    }
-
-                    while (tempI <= tempJ && arr[end] == arr[tempJ]) {
-                        tempJ--;
-                    }
-
-                    int totalElementFromStart = (tempI - start);
-                    int totalElementFromEnd = end - tempJ;
-
-                    numPairs += (totalElementFromStart * totalElementFromEnd);
-
-                    start = tempI;
-                    end = tempJ;
-                }
-            } numTriplets += numPairs;
-        } return numTriplets;
-
-
-    }
-
-
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int arr[] = new int[n];
         takeInput(scanner, arr);
         int x = scanner.nextInt();
-//        System.out.println(findTriplet(arr, x));
-        System.out.println(tripletSum2(arr, x));
-//        System.out.println(tripletSum1(arr, x));
+        System.out.println(findTriplet(arr, x));
     }
 }

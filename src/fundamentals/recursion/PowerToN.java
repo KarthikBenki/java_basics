c89d3632d736297ce799631cd393706a874b40ee
KarthikBenki <karthikmc007@gmail.com>
2023-01-24 06:57:52 +0530

    power of n

---
+++ src/fundamentals/recursion/PowerToN.java
@@ -0,0 +1,26 @@
+package fundamentals.recursion;
+
+public class PowerToN {
+    public static int power(int x, int n) {
+
+        if (n == 0) {
+            return 1;
+        }
+
+        if (n == 1) {
+            return x;
+        }
+
+        int smallOutPut = power(x, n - 1);
+        int output = x * smallOutPut;
+
+        return output;
+
+    }
+
+    public static void main(String[] args) {
+        int power = power(2,4);
+
+        System.out.println(power);
+    }
+}

f2d752dd0ece240847e69564f678530b5ea47923
KarthikBenki <karthikmc007@gmail.com>
2023-01-27 11:01:14 +0530

    array sum using recursion

---
+++ src/fundamentals/recursion/ArraySum.java
@@ -0,0 +1,20 @@
+package fundamentals.recursion;
+
+public class ArraySum {
+    public static void main(String[] args) {
+        int sum = arraySum(new int[]{1, 2, 3, 4, 5});
+        System.out.println(sum);
+    }
+
+    private static int arraySum(int[] input) {
+        if (input.length == 1) return input[0];
+        int smallArray[] = new int[input.length - 1];
+        for (int i = 1; i < input.length; i++) {
+            smallArray[i - 1] = input[i];
+        }
+        int smallOutput = arraySum(smallArray);
+        int output = input[0];
+        output += smallOutput;
+        return output;
+    }
+}

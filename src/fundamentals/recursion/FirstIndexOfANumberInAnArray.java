6711efdbbb75be1f2ac1a2603045a1e9e4e96437
KarthikBenki <karthikmc007@gmail.com>
2023-01-28 14:06:12 +0530

    first index of a number in an array method 2

--- src/fundamentals/recursion/FirstIndexOfANumberInAnArray.java
+++ src/fundamentals/recursion/FirstIndexOfANumberInAnArray.java
@@ -5,10 +5,13 @@ public class FirstIndexOfANumberInAnArray {
 
     public static void main(String[] args) {
         int a[] = {1, 2, 3, 4, 5};
-        int x = 0;
+        int x = 2;
 
         int ans = findFirstIndexOf(a, x);
         System.out.println(ans);
+
+        int ans1 = firstIndex(a,x);
+        System.out.println(ans1);
     }
 
     private static int findFirstIndexOf(int[] a, int x) {
@@ -23,5 +26,16 @@ public class FirstIndexOfANumberInAnArray {
         int fi = findFirstIndexOf(smallArray, x);
         if (fi == -1) return -1;
         else return fi + 1;
+    }
+
+    public static int firstIndex(int a[], int x) {
+        return firstIndex(a, x, 0);
+    }
+
+    private static int firstIndex(int[] a, int x, int i) {
+        if (i == a.length) return -1;
+        if (a[i] == x) return i;
+
+        return  firstIndex(a,x,i+1);
     }
 }

b8782e20d52d6f2329ecc2df68dbe51663390ccc
KarthikBenki <karthikmc007@gmail.com>
2023-01-29 10:31:18 +0530

    replace pi with 3.14

---
+++ src/fundamentals/recursion/strings/ReplacePi.java
@@ -0,0 +1,22 @@
+package fundamentals.recursion.strings;
+
+public class ReplacePi {
+    public static void main(String[] args) {
+        String s = replacePi("apiapipiaapi");//a3.14a3.143.14aa3.14
+        System.out.println(s);
+    }
+
+    private static String replacePi(String s) {
+        if (s.length() == 0) return s;
+
+        if (s.charAt(0) == 'p' && s.charAt(1) == 'i') {
+            String smallOutput = replacePi(s.substring(2));
+            return 3.14 + smallOutput;
+        } else {
+            String smallOutput = replacePi(s.substring(1));
+            return s.charAt(0) + smallOutput;
+        }
+
+
+    }
+}

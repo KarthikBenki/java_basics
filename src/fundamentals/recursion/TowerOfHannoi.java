d4338448968469c49973db3e4f976c0288fb42e5
KarthikBenki <karthikmc007@gmail.com>
2023-01-30 11:07:26 +0530

    tower of hannoi

---
+++ src/fundamentals/recursion/TowerOfHannoi.java
@@ -0,0 +1,21 @@
+package fundamentals.recursion;
+
+public class TowerOfHannoi {
+    public static void main(String[] args) {
+        int n = 3;
+
+        towerOfHanoi(n,'a','b','c');
+    }
+
+    private static void towerOfHanoi(int n, char a, char b, char c) {
+        if (n==0) return;
+        if (n==1) {
+            System.out.println(a+" "+c);
+            return;
+        }
+
+        towerOfHanoi(n-1,a,c,b);
+        System.out.println(a+" "+c);
+        towerOfHanoi(n-1,b,a,c);
+    }
+}

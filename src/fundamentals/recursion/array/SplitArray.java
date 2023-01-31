6fe808d6bdb0d17a0bcf76871cadd0c1a7fe0a5c
KarthikBenki <karthikmc007@gmail.com>
2023-01-31 17:28:27 +0530

    split array

---
+++ src/fundamentals/recursion/array/SplitArray.java
@@ -0,0 +1,27 @@
+package fundamentals.recursion.array;
+
+public class SplitArray {
+    public static boolean splitArray(int input[]) {
+        return splitArray(input,0,0,0);
+
+    }
+
+    public static boolean splitArray(int arr[], int startIndex, int leftArraySum, int rightArraySum) {
+
+
+        if (startIndex == arr.length) {
+            return leftArraySum == rightArraySum;
+        }
+
+        if (arr[startIndex] % 5 == 0) {
+            leftArraySum += arr[startIndex];
+        } else if (arr[startIndex] % 3 == 0) {
+            rightArraySum += arr[startIndex];
+        } else {
+            return splitArray(arr, startIndex + 1, leftArraySum + arr[startIndex], rightArraySum) || splitArray(arr, startIndex + 1, leftArraySum, rightArraySum + arr[startIndex]);
+        }
+
+        return splitArray(arr, startIndex + 1, leftArraySum, rightArraySum);
+    }
+
+}

57475886a64dcdeee83f86c813003255c69406a4
KarthikBenki <karthikmc007@gmail.com>
2023-02-02 10:57:23 +0530

    addition of two fractions

--- src/oops/fraction/Fraction.java
+++ src/oops/fraction/Fraction.java
@@ -2,11 +2,31 @@ package oops.fraction;
 
 public class Fraction {
 
-    private  int numerator;
-    private  int denominator;
+    private int numerator;
+    private int denominator;
 
     public Fraction(int numerator, int denominator) {
+        if (denominator == 0) denominator = 1;
         this.numerator = numerator;
+        this.denominator = denominator;
+        simplify();
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+        simplify();
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public void setDenominator(int denominator) {
+        if (denominator == 0) denominator = 1;
         this.denominator = denominator;
         simplify();
     }
@@ -17,16 +37,31 @@ public class Fraction {
         for (int i = 2; i <= smaller; i++) {
             if (numerator % i == 0 && denominator % i == 0) gcd = i;
         }
-        numerator = numerator/gcd;
-        denominator = denominator/gcd;
+        numerator = numerator / gcd;
+        denominator = denominator / gcd;
     }
 
     public void print() {
-        System.out.println(numerator + "/" + denominator);
+        if (denominator != 1)
+            System.out.println(numerator + "/" + denominator);
+        System.out.println(numerator);
     }
 
     public void increment() {
-        numerator = numerator+denominator;
+        numerator = numerator + denominator;
         simplify();
+    }
+
+    public void add(Fraction f) {
+        numerator = this.numerator * f.denominator + f.numerator * this.denominator;
+        denominator = this.denominator * f.denominator;
+        simplify();
+    }
+
+    public static Fraction add(Fraction f1,Fraction f2){
+        int newNumerator = f1.numerator * f2.denominator
+                + f2.numerator * f1.denominator;
+        int newDenominator = f1.denominator * f2.denominator;
+        return new Fraction(newNumerator,newDenominator);
     }
 }
--- src/oops/fraction/FractionUse.java
+++ src/oops/fraction/FractionUse.java
@@ -2,10 +2,18 @@ package oops.fraction;
 
 public class FractionUse {
     public static void main(String[] args) {
-        Fraction f1 = new Fraction(4,8);
+        Fraction f1 = new Fraction(4, 8);
         f1.print();
         f1.increment();
         f1.increment();
         f1.print();
+        Fraction f2 = new Fraction(1, 2);
+        f1.add(f2);
+        f1.print();
+        Fraction f3 = Fraction.add(f1, f2);
+        Fraction add = Fraction.add(new Fraction(1, 2), new Fraction(1, 2));
+        add.print();
     }
+
+
 }

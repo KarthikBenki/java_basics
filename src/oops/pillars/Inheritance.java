1935f555e91a611bf88d2692ccfe0703d07cf042
KarthikBenki <karthikmc007@gmail.com>
2023-02-04 20:24:51 +0530

    protected modifier usage

--- src/oops/pillars/Inheritance.java
+++ src/oops/pillars/Inheritance.java
@@ -9,27 +9,10 @@ public class Inheritance {
         car1.maxSpeed = 100;
 
         car2.setColor("Brown");
-        car2.maxSpeed=200;
+        car2.maxSpeed = 200;
 
         car1.print();
         car2.print();
-    }
-}
-
-class Vehicle {
-    private String color;
-    int maxSpeed;
-
-    void print() {
-        System.out.println("color " + color + " maxSpeed " + maxSpeed);
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public String getColor() {
-        return color;
     }
 }
 

5cd3ae751d72a756a54847a307dc06c2bd855cb1
KarthikBenki <karthikmc007@gmail.com>
2023-02-05 21:52:25 +0530

    usage of user defined classes in generics

--- src/oops/generics/PairUse.java
+++ src/oops/generics/PairUse.java
@@ -1,5 +1,7 @@
 package oops.generics;
 
+import oops.interfaces.Vehicle;
+
 public class PairUse {
     public static void main(String[] args) {
         Pair<Integer> p = new Pair<>(10, 20);
@@ -10,5 +12,9 @@ public class PairUse {
 
         Pair<Double> doublePair = new Pair<>(3.14, 1.23);
         System.out.println(doublePair.getFirst() + " " + doublePair.getSecond());
+
+        Pair<Vehicle> vehiclePair = new Pair<>(new Vehicle(), new Vehicle());
+        System.out.println(vehiclePair.getFirst() + " " + vehiclePair.getSecond());
+
     }
 }
--- src/oops/interfaces/Vehicle.java
+++ src/oops/interfaces/Vehicle.java
@@ -6,4 +6,9 @@ public class Vehicle {
     public void print(){
         System.out.println("Vehicle");
     }
+
+    @Override
+    public String toString() {
+        return "Vehicle{}";
+    }
 }

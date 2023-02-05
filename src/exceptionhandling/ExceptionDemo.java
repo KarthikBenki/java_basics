f3556ab54176e992e4cd23fdb7df1aecd3adecdd
KarthikBenki <karthikmc007@gmail.com>
2023-02-05 10:02:37 +0530

    format code

--- src/exceptionhandling/ExceptionDemo.java
+++ src/exceptionhandling/ExceptionDemo.java
@@ -1,22 +1,20 @@
 package exceptionhandling;
 
 public class ExceptionDemo {
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
 
-            devideByZero(10,0);
-
-
+        devideByZero(10, 0);
     }
 
-    private static void devideByZero(int numerator, int denominator)  {
-       if (denominator == 0)
-           try {
-               throw new DevideByZeroException("denominator must not be zero");
-           } catch (DevideByZeroException ex) {
-               throw new RuntimeException(ex);
-           }finally {
-               System.out.println("Finally block excecuted");
-           }
-       }
+    private static void devideByZero(int numerator, int denominator) {
+        if (denominator == 0)
+            try {
+                throw new DevideByZeroException("denominator must not be zero");
+            } catch (DevideByZeroException ex) {
+                throw new RuntimeException(ex);
+            } finally {
+                System.out.println("Finally block excecuted");
+            }
+    }
 
 }

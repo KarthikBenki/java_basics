260bafc18e96afde5852d86b4be21ffd696e23e8
KarthikBenki <karthikmc007@gmail.com>
2023-02-06 20:34:25 +0530

    format code

--- src/oops/generics/Print.java
+++ src/oops/generics/Print.java
@@ -2,41 +2,40 @@ package oops.generics;
 
 public class Print {
 
-    public static <T>void printArray(T[] arr) {
+    public static <T> void printArray(T[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
 
-    public static <T extends PrintInterface>void printArray(T[] arr) {
+    public static <T extends PrintInterface> void printArray(T[] arr) {
         for (int i = 0; i < arr.length; i++) {
-           arr[i].print();
+            arr[i].print();
         }
         System.out.println();
     }
 
 
-
     public static void main(String[] args) {
         Integer arr[] = {1, 2, 3, 4, 5};
-       printArray(arr);
+        printArray(arr);
 
         Double dArr[] = {1.14, 2.22, 3.35, 5.46, 9.15};
         printArray(dArr);
 
-        String sArr[] = {"ab","bc","cd","de","ef"};
+        String sArr[] = {"ab", "bc", "cd", "de", "ef"};
         printArray(sArr);
 
         Pair<Integer>[] pArr = new Pair[5];
         for (int i = 0; i < pArr.length; i++) {
-            pArr[i] = new Pair<>(10+i,20+i);
+            pArr[i] = new Pair<>(10 + i, 20 + i);
         }
         printArray(pArr);
 
         Student[] Student = new Student[5];
         for (int i = 0; i < Student.length; i++) {
-            Student[i] = new Student(i+1);
+            Student[i] = new Student(i + 1);
         }
         printArray(Student);
     }

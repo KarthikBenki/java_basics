bb87f88d28cbe3b0485350ed80a75efa98432bc2
KarthikBenki <karthikmc007@gmail.com>
2023-02-07 04:46:23 +0530

    basic array list structure

---
+++ src/dsa/arraylist/TempArrayList.java
@@ -0,0 +1,22 @@
+package dsa.arraylist;
+
+import java.util.ArrayList;
+
+public class TempArrayList {
+
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("apple");
+        list.add("banana");
+        list.add("carrot");
+        list.add(0, "mango");//adds new data pushes the existing data to next place
+
+        System.out.println(list.get(2));
+
+        list.stream().forEach(a -> System.out.print(a + " "));
+
+
+
+
+    }
+}

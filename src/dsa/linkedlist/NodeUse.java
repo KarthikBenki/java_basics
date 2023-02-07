801f15f970cfa3d4f2919e8c91df27327badfbe1
KarthikBenki <karthikmc007@gmail.com>
2023-02-07 05:48:03 +0530

    build linked list and print

---
+++ src/dsa/linkedlist/NodeUse.java
@@ -0,0 +1,30 @@
+package dsa.linkedlist;
+
+public class NodeUse {
+    public static Node<Integer> createLinkedList() {
+        Node<Integer> n1 = new Node<>(10);
+        Node<Integer> n2 = new Node<>(20);
+        Node<Integer> n3 = new Node<>(30);
+        Node<Integer> n4 = new Node<>(40);
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+
+        return n1;
+    }
+
+    public static void main(String[] args) {
+        Node<Integer> head = createLinkedList();
+
+        printLinkedList(head);
+
+    }
+
+    private static void printLinkedList(Node<Integer> linkedList) {
+        while (linkedList.next != null) {
+            System.out.print(linkedList.data + " ");
+            linkedList = linkedList.next;
+        }
+        System.out.println(linkedList.data);
+    }
+}

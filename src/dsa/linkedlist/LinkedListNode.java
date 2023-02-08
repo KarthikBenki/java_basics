f105cbeaf1b33e7fbdcb197d825dbcc31bb1a35e
KarthikBenki <karthikmc007@gmail.com>
2023-02-08 20:00:47 +0530

    made properties public

--- src/dsa/linkedlist/LinkedListNode.java
+++ src/dsa/linkedlist/LinkedListNode.java
@@ -1,8 +1,8 @@
 package dsa.linkedlist;
 
 public class LinkedListNode<T> {
-    T data;
-    LinkedListNode<T> next;
+   public T data;
+   public LinkedListNode<T> next;
 
     public LinkedListNode(T data) {
         this.data = data;

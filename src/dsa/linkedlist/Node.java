a7e77bb06bc0751f4d504fe3008157ad78b87c41
KarthikBenki <karthikmc007@gmail.com>
2023-02-08 19:45:58 +0530

    created node class

---
+++ src/dsa/linkedlist/Node.java
@@ -0,0 +1,11 @@
+package dsa.linkedlist;
+
+public class Node {
+    private int data;
+    private Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
